ce8bdb23e47843ea7582351ef9361cb71f975ad4
Aaryan2901 <aaryangupta104@gmail.com>
2022-07-24 22:55:51 +0530

    changes

---
+++ src/components/Information.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Information = () => {
+  return (
+    <><div style={{display: "grid"}}>
+      Enter your name:-<input type='text' />
+      Enter your Email address:-<input type='text' />
+      Enter your room number:-<input type='text' />
+    </div>
+    <div>
+        <button>Submit</button>
+    </div>
+    </>)
+}
+
+export default Information
